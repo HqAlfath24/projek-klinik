6f224453f405c8ca190021e031288bb45bab20b9
HqAlfath24 <m.hq24alfath@gmail.com>
2024-05-20 16:01:58 +0700

    membuat tabel treatent untuk fasilitas tiap poli

---
+++ app/Database/Migrations/2024-05-20-085519_Treatment.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Treatment extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_poli' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name_poly' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'poly_code' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => TRUE,
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME',
+                'null'       => TRUE,
+            ],
+            'deleted_at' => [
+                'type'       => 'DATETIME',
+                'null'       => TRUE,
+            ],
+        ]);
+        $this->forge->addKey('id_poli', true);
+        $this->forge->createTable('policlinics');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('policlinics');
+    }
+}
